b23d343151142ea2cdcd29c7e6d7562007f51d14
Ilya Beliaev <info@php-dev.info>
2025-08-02 01:01:09 +0200

    .

--- src/Adapter/WordpressCron.php
+++ src/Adapter/WordpressCron.php
@@ -2,17 +2,67 @@
 
 declare(strict_types=1);
 
-namespace N3XT0R\XPub\Adapter;
+namespace WP\Skeleton\Adapter;
+
+use DI\Container;
 
 final class WordpressCron
 {
+
+    public const CRON_HOOK = 'skeleton_job';
+    public const CRON_SCHEDULE = 'skeleton_every_minute';
+    private const CRON_INTERVAL = 60;
+
+    private static ?Container $container = null;
+
+    public static function setContainer(Container $container): void
+    {
+        self::$container = $container;
+    }
+
+    private static function container(): Container
+    {
+        if (self::$container === null) {
+            throw new \RuntimeException('Container not initialized');
+        }
+
+        return self::$container;
+    }
+
     public function register(): void
     {
         // TODO: register cron events
     }
 
-    public function handle(): void
+    public static function schedule(): void
     {
-        // TODO: handle cron execution
+        if (!self::isRegistered()) {
+            wp_schedule_event(time(), self::CRON_SCHEDULE, self::CRON_HOOK);
+        }
+    }
+
+    public static function unschedule(): void
+    {
+        $timestamp = wp_next_scheduled(self::CRON_HOOK);
+        if ($timestamp !== false) {
+            wp_unschedule_event($timestamp, self::CRON_HOOK);
+        }
+    }
+
+    public static function isRegistered(): bool
+    {
+        return wp_next_scheduled(self::CRON_HOOK) !== false;
+    }
+
+    public static function init(): void
+    {
+        self::container();
+        add_filter('cron_schedules', [self::class, 'addSchedule']);
+        add_action('skeleton_some_job', [self::class, 'run']);
+    }
+
+    public static function run(): void
+    {
+
     }
 }
--- src/Adapter/WordpressPlugin.php
+++ src/Adapter/WordpressPlugin.php
@@ -2,28 +2,34 @@
 
 declare(strict_types=1);
 
-namespace N3XT0R\XPub\Adapter;
+namespace WP\Skeleton\Adapter;
 
 use WP\Skeleton\Infrastructure\DI\ContainerProvider;
 use WP\Skeleton\Shared\Plugin\PluginContext;
 
+use DI\Container;
+
 final class WordpressPlugin
 {
-    private function __construct()
+    private static ?Container $container = null;
+
+    public static function setContainer(Container $container): void
     {
+        self::$container = $container;
+    }
+
+    private static function container(): Container
+    {
+        if (self::$container === null) {
+            throw new \RuntimeException('Container not initialized');
+        }
+
+        return self::$container;
     }
 
     public static function init(string $pluginFile): void
     {
-        ContainerProvider::setPluginContext(
-            new PluginContext(
-                $pluginFile,
-                'wp-modern-plugin-skeleton',
-                'https://example.com'
-            )
-        );
-
-        // Build the container. Services can now be fetched via ContainerProvider
-        ContainerProvider::getContainer();
+        self::container();
+        //do some things
     }
 }
